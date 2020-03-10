4fc933f0db345867bea0b8e23dd5d156852ce15c
alokkarnik <alokkrnk@gmail.com>
2020-03-10 13:46:14 +0530

    Add user input mechanism

--- Since/Controller/ViewController.swift
+++ Since/Controller/ViewController.swift
@@ -8,14 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var addButton: UIButton!
 
+    @IBOutlet weak var activityInputView: UIView!
+    
+    @IBOutlet weak var activityTitleTextField: UITextField!
+    @IBOutlet var tableViewBottomConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var insertActivity: UIButton!
+    
     var activityData: [Activity]? = nil
     var storageController = ActivityStorageController()
 
+    @IBAction func add(_ sender: Any) {
+         showAddActivityView()
+     }
+
+     func showAddActivityView() {
+         addButton.isHidden = true
+         activityInputView.isHidden = false
+         tableViewBottomConstraint.isActive = false
+         activityTitleTextField.becomeFirstResponder()
+     }
+
+     func showDefaultView() {
+         addButton.isHidden = false
+         activityInputView.isHidden = true
+         tableViewBottomConstraint.isActive = true
+     }
+    
+     @IBAction func insertActivity(_ sender: Any) {
+         if let activityTitle = activityTitleTextField.text {
+             storageController.insertActivity(activityTitle: activityTitle)
+             
+             addButton.isHidden = false
+             activityInputView.isHidden = true
+             tableViewBottomConstraint.isActive = true
+             reloadData()
+             dismissKeyboard()
+         }
+     }
+
+    func reloadData() {
+        activityData = storageController.getAllActivities()
+        tableView.reloadData()
+    }
+}
+
+
+extension ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,8 +68,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
         tableView.contentInset = UIEdgeInsets(top: 25, left: 0, bottom: 0, right: 0)
         tableView.allowsSelection = false
+        activityInputView.isHidden = true
 
         activityData = storageController.getAllActivities()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(screenTaped))
+        view.addGestureRecognizer(tap)
     }
 
     override func viewDidLayoutSubviews() {
@@ -35,10 +87,33 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         addButton.clipsToBounds = true
         addButton.titleLabel?.textAlignment = .center
     }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.size.height {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+
+    @objc func screenTaped() {
+        dismissKeyboard()
+        showDefaultView()
+    }
 }
 
 
-extension  ViewController {
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SinceCell") as! TableViewCell
         
@@ -57,12 +132,5 @@ extension  ViewController {
         }
         return allActivities.count
     }
-    
-    @IBAction func add(_ sender: Any) {
-        storageController.insertActivity(activityTitle: "someActivity \(activityData?.count ?? 0)")
-        activityData = storageController.getAllActivities()
-
-        tableView.reloadData()
-    }
-
 }
+
