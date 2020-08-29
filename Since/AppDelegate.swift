1ff2f1cb17f0ac550a01c57aa00668a422173b36
alok <alokkrnk@gmail.com>
2020-08-29 14:35:06 +0530

    Force light mode

--- Since/AppDelegate.swift
+++ Since/AppDelegate.swift
@@ -13,7 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        if #available(iOS 13.0, *) {
+            window?.overrideUserInterfaceStyle = .light
+        }
         return true
     }
 }
